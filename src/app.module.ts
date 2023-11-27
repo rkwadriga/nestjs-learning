7626c406125ceb4ad9a10b276276618c45351a29
rkwadriga <dmitry.kushneriov@obreey-products.com>
2023-11-27 17:10:23 +0200

    * Install @nestjs/config * Install @nestjs/mongoose * Install mongoose * Create test models "User" and "Post"

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,29 @@ import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from "@nestjs/mongoose";
+import { UsersModule } from './users/users.module';
+
+const host = process.env.MONGO_DB_HOST;
+const port = process.env.MONGO_DB_PORT;
+const user = process.env.MONGO_DB_USERNAME;
+const pass = process.env.MONGO_DB_PASSWORD;
+const dbname = process.env.MONGO_DB_DBNAME;
 
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      //`mongodb://${user}:${pass}@${host}:${port}/${dbname}`,
+      'mongodb://top:top@localhost:27018/top_api_db',
+    ),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './models/user.model';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  public async getByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  public async createuser({email, password}: {email: string, password: string}) {
+    const newUser = new this.userModel({email, password});
+
+    return newUser.save();
+  }
+}
